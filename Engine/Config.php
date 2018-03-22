4ad2bf3ca5a2130330cd61a9e34fffbb97b376ea
Serge Kishiko <sergekishiko@yahoo.fr>
2018-03-22 13:06:14 +0200

    Reset database Config values

--- Engine/Config.php
+++ Engine/Config.php
@@ -12,10 +12,10 @@ final class Config
 {
     // Database info (if you want to test the script, please edit the below constants with yours)
     const
-    DB_HOST = 'localhost',
+    DB_HOST = 'mymvcblog',
     DB_NAME = 'mymvcblog',
     DB_USR = 'root',
-    DB_PWD = '',
+    DB_PWD = 'pwd123',
 
     // Title of the site
     SITE_NAME = 'My Simple Blog!';
